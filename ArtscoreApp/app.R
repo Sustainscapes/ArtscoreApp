6984131763381eb78fa51c306cac0a5c4ce2e1a8
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2022-01-26 22:20:01 +0100

    App Ready too artscore

--- ArtscoreApp/app.R
+++ ArtscoreApp/app.R
@@ -12,12 +12,14 @@ library(tidyverse)
 library(readxl)
 library(janitor)
 
+library(readxl)
+
 Habitattypernes_scoringer <- read_excel("Habitattypernes scoringer og vægtninger_lysåben og skov.xlsx",
                                           sheet = "bilag 1 artsscorer") %>% 
     clean_names() %>% 
     dplyr::mutate(dansk_navn = str_remove_all(dansk_navn, "\\r\\n"), 
                   videnskabeligt_navn = str_remove_all(videnskabeligt_navn, "\\r\\n")) %>% 
-    pivot_longer(stenstrand_12:skov_91, names_to = "Habitat")
+    pivot_longer(stenstrand_12:skov_91, names_to = "Habitat", values_to = "Artscore")
     
 
 Habitat_types <- read_excel("Habitattypernes scoringer og vægtninger_lysåben og skov.xlsx", 
@@ -43,7 +45,7 @@ Habitat_types <- read_excel("Habitattypernes scoringer og vægtninger_lysåben o
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+    withMathJax(),
     # Application title
     titlePanel("Artscore calculation"),
 
@@ -66,8 +68,10 @@ ui <- fluidPage(
         ),
 
         # 3 Filtered
-        mainPanel(
-           dataTableOutput("SpeciesScores")
+        mainPanel(h2("Raw data"),
+           dataTableOutput("SpeciesScores"),
+           h2("Calculated values"),
+           dataTableOutput("Table2")
         )
     )
 )
@@ -112,7 +116,7 @@ server <- function(input, output) {
     
 ## 3 Filtered
     
-    output$SpeciesScores <- renderDataTable({
+    Table1 <- reactive({
         if(get_choice() == 1){
             req(input$SpeciesListSc)
             Results <- Habitattypernes_scoringer %>% 
@@ -139,6 +143,41 @@ server <- function(input, output) {
             dplyr::filter(Habitat == SelectHabitat)
         Results
     })
+    
+    
+    output$SpeciesScores <- renderDataTable({
+      Table1()
+    })
+    
+    middelscore <- reactive(mean(Table1()$Artscore))
+    Summscore <- reactive(sum(Table1()$bilagsart_1_ikke_bilagsart_0))
+    
+    HabitatScores <- reactive({
+        if(Habs() == 1){
+            req(input$HabsCode)
+            SelectHabitat <- Habitat_types %>% 
+                dplyr::filter(habitatnaturtype %in% input$HabsCode)
+        }
+        else if(Habs() == 2){
+            req(input$HabsName)
+            SelectHabitat <- Habitat_types %>% 
+                dplyr::filter(habitat_name %in% input$HabsName)
+        }
+    })
+    
+    output$Table2 <- renderDataTable({
+        data.frame(Symbol = c("m", "a(b)", "s", "m(a)", "A(s)", "a(t)", "n(a)", "d", "A(d)", NA), 
+                   Beskrivelse = c("Justeret middelscore for prøvefeltet", "Antal (a) bidragsarter (b) (antal med artsscore over 0).", "Artssum", "Gennemsnit justeret middel score", "Artssscoreindex", "antal plantearter i prøvefeltet (uden mosser mm)", "middel artsantal i naturtypen", "diversitetsparameter", "Artsdiversitetsindex", "Artsindex (vægtning af artscore- og artsdiversitetsindeks)"), 
+                   Formel = c(NA, NA, "m*a(b)", NA, "A(s) = 1/((1+exp(m(a)))*exp(1,60(1-m)))", NA, NA, "d = 0,8*m(a)*n(a)", "A(d) = (a(b)/a(t))*(1-(1/exp(s/d)))", "0,75*A(s)+0,25*A(d)"), 
+                   Scores = c(middelscore(), Summscore(), middelscore()*Summscore(), HabitatScores()$gennemsnitlig_middelscore,
+                              1/((1+exp(HabitatScores()$gennemsnitlig_middelscore))*exp(1.60*(1-middelscore()))), 
+                              nrow(Table1()),
+                             HabitatScores()$gennemsnitligt_artsantal,
+                             HabitatScores()$gennemsnitlig_middelscore*HabitatScores()$gennemsnitligt_artsantal*0.8,
+                             (Summscore()/nrow(Table1()))*(1-(1/exp((middelscore()*Summscore())/(HabitatScores()$gennemsnitlig_middelscore*HabitatScores()$gennemsnitligt_artsantal*0.8)))),    
+                             0.75*(1/((1+exp(HabitatScores()$gennemsnitlig_middelscore))*exp(1.60*(1-middelscore()))))+0.25*((Summscore()/nrow(Table1()))*(1-(1/exp((middelscore()*Summscore())/(HabitatScores()$gennemsnitlig_middelscore*HabitatScores()$gennemsnitligt_artsantal*0.8)))))))
+    })
+    #
 }
 
 # Run the application 
